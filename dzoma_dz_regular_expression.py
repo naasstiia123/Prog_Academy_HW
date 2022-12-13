07b8299317701924978661b60cebc65197a12269
Anastasiia Dzoma <102649388+naasstiia123@users.noreply.github.com>
2022-12-13 17:26:14 +0200

    Update dzoma_dz_regular_expression.py

--- dzoma_dz_regular_expression.py
+++ dzoma_dz_regular_expression.py
@@ -56,26 +56,29 @@ def check(email: str):
     :param email: email
     :return: Boolean answer
     '''
-
-    if not isinstance(email, str):
+if not isinstance(email, str):
         raise TypeError
-    patterns = (r'^[^\W\s_]+', r'[0-9]+', r'[A-Z]+', r'[a-z]+', r'_*', r'-?')
-    pattern_2 = r'^[^\W\s_]+-{1,}[^\W\s]+-{1,}'
-    pattern_3 = r'[^\w\s-]'
+    patterns = (r'^[^\W\s_]+', r'[0-9]+', r'[A-Z]+', r'[a-z]+', r'_*', r'-?', r'@{1}\w+\.{1}\w+$')
+    pattern_2 = r'-{1,}[\w\W]+-{1,}'
+    pattern_3 = r'@+[\w\W]+@+'
+    pattern_4 = r'\.+[\w\W]+\.+'
+    pattern_5 = r'[^\w\s@.-]'
     for item in patterns:
         check_1 = re.search(item, email)
         if not check_1:
             return False
     check_2 = re.search(pattern_2, email)
     check_3 = re.search(pattern_3, email)
-    if check_2 or check_3:
+    check_4 = re.search(pattern_4, email)
+    check_5 = re.search(pattern_5, email)
+    if check_2 or check_3 or check_4 or check_5:
         return False
 
     return True
 
-a = check('8bH-olla4__')
+a = check('8bH-olla4__@gmail.com')
 print(a)
-
+    
 
 #4
 def check(login):
